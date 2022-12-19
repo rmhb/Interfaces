3c7ae2cc4828c4883e7e425e307a09a6feb2cdec
Rosa <rosa.hb84@gmail.com>
2022-12-19 19:09:07 +0100

    Rental con Tips y shortscuts

--- src/main/java/com/dam/rental/RentCarController.java
+++ src/main/java/com/dam/rental/RentCarController.java
@@ -25,12 +25,13 @@ public class RentCarController implements Initializable {
     Label _label1,_label2, _label3, _label4, _label5, _label6, _label7, _label8, _label9;
       // Probar con bbagg6@tmall.com   vlVO3U
     List<Label> labels;
-    String[] tablero;
+
+    private int matriz[][];
     private final int J1 = 1;
     private final int J2 = 2;
     private int player, winner;
     private Integer[] scores;
-    private Integer[] casillaEstaLibre;
+
 
     
 
@@ -61,7 +62,6 @@ public class RentCarController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        tablero = new String[9];
         labels = new ArrayList<Label>();
         labels.add(_label1);
         labels.add(_label2);
@@ -75,20 +75,14 @@ public class RentCarController implements Initializable {
         player = 0;
         winner = 0;
         scores = new Integer[9];
-        casillaEstaLibre = new Integer[9];
         Arrays.fill(scores,0);
-        Arrays.fill(casillaEstaLibre,1);       
+
     }   
-       private String[] getActualState(){           
-        for (int i = 0; i < 9; i++) {
-            tablero[i] = labels.get(i).getText();
-        }
-        return tablero;
-    }
+
     private boolean  isBoardFull(){
         boolean completo = true;
         for (int i = 0; i < 9; i++) {
-            if (tablero[i].isBlank()){
+            if (labels.get(i).getText().isBlank()){
                 completo = false;
                 break;
             }
@@ -101,9 +95,9 @@ public class RentCarController implements Initializable {
         for (int r = 0; r < 9; r+=3) {
 //            System.out.println("VAlor r "+r);
             
-            if (tablero[0+r].contentEquals(tablero[1+r]) && tablero[0+r].contentEquals(tablero[2+r]) && !tablero[r].isBlank() ) {
+            if (labels.get(0+r).getText().contentEquals(labels.get(1+r).getText()) && labels.get(0+r).getText().contentEquals(labels.get(2+r).getText()) && !labels.get(0+r).getText().isBlank() ) {
                 lineaWin = r/3;
-                if(tablero[0].contentEquals("X"))
+                if(labels.get(0+r).getText().contentEquals("X"))
                     winner = J1;
                 else
                     winner = J2;
@@ -117,9 +111,9 @@ public class RentCarController implements Initializable {
         for (int c = 0; c < 3; c++) {
 //            System.out.println("VAlor r "+c);
 
-            if (tablero[c].contentEquals(tablero[c+3]) && tablero[c].contentEquals(tablero[c+6]) && !tablero[c].isBlank() ) {
+            if (labels.get(c).getText().contentEquals(labels.get(c+3).getText()) && labels.get(c).getText().contentEquals(labels.get(c+6).getText()) && !labels.get(c).getText().isBlank() ) {
                 colWin = c;
-                if(tablero[c].contentEquals("X"))
+                if(labels.get(c).getText().contentEquals("X"))
                     winner = J1;
                 else
                     winner = J2;
@@ -133,16 +127,16 @@ public class RentCarController implements Initializable {
         for (int c = 0; c < 3; c++) {
 //            System.out.println("VAlor r "+c);
 
-            if (tablero[0].contentEquals(tablero[4]) && tablero[0].contentEquals(tablero[8]) && !tablero[0].isBlank() ) {
+            if (labels.get(0).getText().contentEquals(labels.get(4).getText()) && labels.get(0).getText().contentEquals(labels.get(8).getText()) && !labels.get(0).getText().isBlank() ) {
                 diagonalWin = 1;
-                if(!tablero[0].contentEquals("X"))
-                    winner = J2;
-                else
+                if(labels.get(0).getText().contentEquals("X"))
                     winner = J1;
+                else
+                    winner = J2;
                 
-            }else if(tablero[2].contentEquals(tablero[4]) && tablero[2].contentEquals(tablero[6]) && !tablero[2].isBlank() ) {
+            }else if(labels.get(2).getText().contentEquals(labels.get(4).getText()) && labels.get(2).getText().contentEquals(labels.get(6).getText()) && !labels.get(2).getText().isBlank() ) {
                 diagonalWin = 2;
-                if(tablero[2].contentEquals("X"))
+                if(labels.get(2).getText().contentEquals("X"))
                     winner = J1;
                 else
                     winner = J2;
@@ -156,15 +150,16 @@ public class RentCarController implements Initializable {
      * Ficha va a reprsentar la alternancia entre las X y las O
      * profuncidad... cuanto nos queda para acabar, es decir cuanta recursividad hemos tenido qeu agotar para ganar. Profundidad 9 porque tengo 9 casillas
      */
-    private int evalua(String ficha, int profundidad ){
+    private int evalua(String ficha, int profundidad ) throws InterruptedException{
 // Ficha = 1 si eres el PC ; Ficha = -1 si eres el jugador
 // La máxima puntuación que le vamos a dar es la profundidad y la mínima el opuesto : -profundidad. Basicamente en número de jugadas en las que ganamos.
 // Como voy a ir actualizando la puntuación y quedandome con la mejor, voy a partir de la peor puntuación posible en este caso -profundidad. Asi tendre que las puntuaciones negativas corresponden con que el usuario ha ganado y las positivas son las que yo he ganado. La mínima sería al revés parte del máximo y me voy queedando con la peor. 
 // La minima puntuación la quiero porque al invertir el turno la puntuación del contraio es la inversa de la del turno actual. Si soy el PC para poder ganar me voy a poner en el caso peor y la siguiente jugada del jugador voy a suponer que es la mejor.
-        int puntuacion = 0;
+        int puntuacion;
         int maxPuntuacion = -profundidad;
         int minPuntuacion = profundidad;
         boolean stopLooking = false;
+        
         if(weHaveWinner()){
             if(ficha == "X")
                 return profundidad; // El winner es el pc
@@ -182,16 +177,16 @@ public class RentCarController implements Initializable {
              // iNVERSION DE FICHA.
             if( ficha == "X") ficha = "O";
             else ficha = "X";
-            
+
             int i = 0;
             while(!stopLooking && i<9){
-                //System.out.println("Tablero is blanck check i= " +i+" "+ tablero[i].isBlank());
-                if (tablero[i].isBlank()){
+                if (labels.get(i).getText().isBlank()){
                     labels.get(i).setText(ficha); // Poner ficha para evaluar
-                    //System.out.println("He puesto ficha "+ficha+" en posicion "+ i + " en profundidad " + profundidad);
+                    // System.out.println("EVALUANDO prof= "+profundidad+", casilla= "+i+", ficha= "+ficha);
                     puntuacion = evalua(ficha,profundidad-1);
-                    labels.get(i).setText(""); // Quitar ficha
-                    
+                    // System.out.println("RESULTADO prof= "+profundidad+", casilla= "+i+", ficha= "+ficha+". PUNT= "+puntuacion);
+                    labels.get(i).setText(" "); // Quitar ficha
+
                     // Si la puntuacion de la casilla actual es mayor que la de todas las anteriores
                     if (puntuacion > maxPuntuacion)  {maxPuntuacion= puntuacion;} // Toma esta puntuaci�n como la mayor de todas
                     // Si la puntuacion de la casilla actual es menor que la de todas las anteriores
@@ -202,6 +197,7 @@ public class RentCarController implements Initializable {
                     // Deja de evaluar columnas si ha encontrado la mejor de todas las posibles jugadas para el ordenador. Si el PC ya gana no sigo mirando, ya tengo la mejor.
                     else  {stopLooking = (puntuacion==profundidad-1);} 
                 }
+                //else {System.out.println("SKIPPING POR CASILLA OCUPADA! prof= "+profundidad+", casilla= "+i+", ficha= "+ficha+". Hay puesta una "+labels.get(i).getText());}
                 i++;
             }
            
@@ -212,59 +208,59 @@ public class RentCarController implements Initializable {
         }
     }
 
-    @Override
-    public String toString() {
-        return "RentCarController{" + "tablero=" + tablero + '}';
-    }
-    
-    
+       
     
     @FXML
-    public void userClick(MouseEvent event){
+    public void userClick(MouseEvent event) throws InterruptedException{
+        boolean tablas=false, victoria=false, derrota=false;
+        
+        
         Label label = (Label) event.getSource();
         String botonPulsado = label.getId();
         if (label.getText().isBlank()){
             label.setText("O");
-            if(weHaveWinner()){
-                System.out.println("El usuario ha ganado");
-            }
-            else if (isBoardFull()){
-                System.out.println("Tenemos tablas");
-            }
-            else{
-                
+            
+            if(weHaveWinner()) {System.out.println("USER WINS"); derrota=true;}
+            else if (isBoardFull()) {System.out.println("TABLAS"); tablas=true;}
+            if (!(derrota || tablas))
+            {
                 Arrays.fill(scores,0);
-                Arrays.fill(casillaEstaLibre,1);  
                 // Hacemos el primer movimiento del PC.
                 for (int i = 0; i < 9; i++) {
                     if (labels.get(i).getText().isBlank())
                      {
-                       labels.get(i).setText("X"); // Poner ficha para evaluar                       
-                       scores[i]= evalua("X", 5-1);
-                       System.out.println("puntuación de la casilla "+i+" es "+ scores[i] );
-                       labels.get(i).setText(""); // Quitar ficha
-                         //System.out.println(" Comrpbacion "+ labels.get(i).getText().isBlank());
+                       labels.get(i).setText("X"); // Poner ficha para evaluar
+                       scores[i]= evalua("X", 6-1);
+                       // si llamo con profundidad= 2-1 => caso base tiene 2 fichas puestas de las 9 posibles
+                       // si llamo con profundidad= 3-1 => caso base tiene 3 fichas puestas de las 9 posibles
+                       // ...
+                       // si llamo con profundidad= 9-1 => caso base tiene 9 fichas puestas de las 9
+
+                       // a partir de profundidad= 7-1 ya es imposible ganarle :)
+                       // Probar con la apertura casilla0="O". La máquina solo puede forzar tablas con casilla5="X" (Si no ocupa esa casilla, la ocupa el usuario y lo tiene ganado)
+
+                       //System.out.println("puntuación de la casilla "+i+" es "+ scores[i] );
+                       labels.get(i).setText(" "); // Quitar ficha
+                       //System.out.println(" Comrpbacion "+ labels.get(i).getText().isBlank());
                      }
-                    else {casillaEstaLibre[i]=0;}
+                    else {scores[i]=-9999;} //valor exageradamente negativo para indicar que esa casilla está ocupada (=> no es una opción de jugada evaluable!)
+                    // otra posibilidad es usar otro array adicional para guardar los índices de las casillas libres
+                    // otra posibilidad es usar algún LinkedList, pero para eso hay que conocerlos :)
                      
                 }
                 
-                int primeraCasillaLibre=0;
-                while (casillaEstaLibre[primeraCasillaLibre]==0)
-                 {
-                   primeraCasillaLibre++;    
-                 }
-                
-                int mejorCasilla=primeraCasillaLibre;         
+                System.out.println(Arrays.toString(scores));
+                int mejor_movimiento= 0;
                 for (int casilla = 0; casilla < scores.length; casilla++)
                  {
-                   if (casillaEstaLibre[casilla]==1)
-                    {
-                      mejorCasilla= scores[casilla] > scores[mejorCasilla] ? casilla : mejorCasilla;
-                    }
+                   mejor_movimiento= scores[casilla] > scores[mejor_movimiento] ? casilla : mejor_movimiento;
                  }
          
-                labels.get(mejorCasilla).setText("X");
+                labels.get(mejor_movimiento).setText("X");
+                
+                
+                if(weHaveWinner()) {System.out.println("COMPUTER WINS"); victoria=true;}
+                else if (isBoardFull()) {System.out.println("TABLAS"); tablas=true;}
             }
             
         }
@@ -278,14 +274,13 @@ public class RentCarController implements Initializable {
 //        System.out.println(label.getTextFill());
 //        label.setTextFill(Color.RED);
         boolean isWinner = false;        
-        tablero = getActualState();
         
         int winnerLine = checkLine();
         int winnerCol = checkCol();
         int winnerDiagonal = checkDiagonal();
 //      System.out.println("Linea ganadora "+winnerLine);
-//        System.out.println("Columna ganadora "+winnerCol);
-//        System.out.println("Diagonal ganadora "+winnerDiagonal);
+//       System.out.println("Columna ganadora "+winnerCol);
+//       System.out.println("Diagonal ganadora "+winnerDiagonal);
         if(winnerLine != -1 || winnerCol !=-1 || winnerDiagonal !=-1){
             isWinner = true;
         }
